c8a75b5a8c08b8c370780d7fa4aa10a8b6bb0a0f
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-22 23:48:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/.{5}/)
+  text.scan(/\bun+[a-z]+ing/)
 end
 
 def words_five_letters_long(text)
-  test.scan(/[a-z]{5}/)
+  test.scan(/.{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
