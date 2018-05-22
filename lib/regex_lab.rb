b7fcd7b836b9c5c2056c6fb0937d5d4a3aa165e8
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-22 23:31:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/\b[aeiou]/)
+  if word.scan(/\b[aeiou]/).length >= 1
     return true
   end
 end
@@ -9,7 +9,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
